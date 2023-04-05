f3fe846e38f556d6aa824ab3d6440a6dde8d981d
Subu Kandaswamy <b.kandaswamy@wsu.edu>
2023-04-05 11:48:30 -0700

    Consuming rest api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Course from "./components/Course";
 
 function App() {
+  const coursesVal = [
+    {
+      courseid: "CPTS421",
+      coursename: "Capstone",
+    },
+    {
+      courseid: "CPTS489",
+      coursename: "Web Development",
+    },
+  ];
+
+  const [courses, setcourses] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/api/courses")
+      .then((res) => res.json())
+      .then((data) => setcourses(data))
+      .catch(console.error);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to CMS!</h1>
+      {courses.map((course) => {
+        return (
+          <Course courseid={course.courseid} coursename={course.coursename} />
+        );
+      })}
     </div>
   );
 }
